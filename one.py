9a4f95123881964fb2bfbe7bca89a753e445f04c
Amirova Milyausha <77613038+AMilyausha@users.noreply.github.com>
2023-11-07 13:00:13 +0300

    Add files via upload

--- one.py
+++ one.py
@@ -1,17 +1,10 @@
-def sortMassivImperative(arr):
-    flag = True
-    while flag:
-        flag = False
-        for i in range(len(arr) - 1):
-            if arr[i] < arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                flag = True
-    return arr
+def multiply_table(n):
+    table = []
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+             print(i,'*',j,'=',i*j)     
+ 
 
-def sortMassivDeclarative(arr):
-    arr.sort(reverse=True)
-    return arr
+n = int(input("Введите число n: "))
+table = multiply_table(n)
 
-
-print(f"Imperative style -> {sortMassivImperative([35, 9, 2, 7, -4, 2, 3, 78])}")
-print(f"Declarative style -> {sortMassivDeclarative([35, 9, 2, 7, -4, 2, 3, 78])}")
